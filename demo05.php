7ba01f75b7ef7f16cdb9fb8a6b829073d1c508ef
Bryan Dolhin <dolhbry@ndlp.fr>
2023-01-11 13:59:01 +0100

    Index

---
+++ demo05.php
@@ -0,0 +1,16 @@
+<HTML>
+<body>
+    <form method="POST">
+        NAME : <input type="text" name="fname">
+        <input type="submit">
+    </form>
+    <?php
+    
+    ?>
+
+</body>
+
+
+
+
+</HTML>
