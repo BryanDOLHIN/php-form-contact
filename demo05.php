2e2f966d05c30d70951a87f059a8156705132d4d
Bryan Dolhin <dolhbry@ndlp.fr>
2023-01-11 14:10:04 +0100

    welcome_post

--- demo05.php
+++ demo05.php
@@ -1,11 +1,23 @@
 <HTML>
 <body>
-    <form method="POST">
-        NAME : <input type="text" name="fname">
+    <form method="POST" action="welcome_post.php">
+        NAME : <input type="text" name="fname"><br>
+        EMAIL : <input type="text" name="email"><br>
         <input type="submit">
     </form>
-    <?php
-    
+    <?php 
+    if ($_SERVER["REQUEST_METHOD"]=="POST")
+    {
+        $name = $_POST['fname'];
+        if (empty($name))
+        {
+            print("Name is empty");
+        }
+        else
+        {
+            print($name);
+        }
+    }
     ?>
 
 </body>
---
+++ welcome_post.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+    Welcome <?php print($_POST["fname"]);?><br>
+    Your email address is : <?php print($_POST['email']);?>
+</body>
+</html>
